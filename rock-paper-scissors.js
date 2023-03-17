d2a3a84c57d17ca7bf57a1d2dfcea4d0d586b557
Brittany T <>
2023-03-17 14:00:09 -0500

    game is scoring but not updating lol

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,4 +1,7 @@
 const choices = ["rock", "paper", "scissors"];
+let playerScore = 0; 
+let computerScore = 0;
+
 
 function getComputerChoice() {
     const choices = ["rock", "paper", "scissors"];
@@ -14,8 +17,22 @@ function playRound(playerSelection) {
    } else if ((playerSelection === "rock" && computerSelection === "scissors") ||
               (playerSelection === "paper" && computerSelection === "rock") ||
               (playerSelection === "scissors" && computerSelection === "paper")) {
+    playerScore++; 
     document.getElementById("result_message").innerHTML = "You WIN! You chose " + playerSelection + " and the computer chose " + computerSelection + ".";
+          if (playerScore === 5) {
+            document.getElementById("winner_message").innerHTML = "You won the game! Refresh to play again.";
+          }
+
    } else {
+    computerScore++;
     document.getElementById("result_message").innerHTML = "You LOSE! You chose " + playerSelection + " and the computer chose " + computerSelection + ".";
    }
+        if (computerScore === 5) {
+            document.getElementById("loser_message").innerHTML = "The Computer won the game! Refresh to play again.";
+        }
+}
+
+function updateScore() {
+  document.getElementById('player_score').innerHTML = "Player: '${playerScore}'";
+  document.getElementById('computer_score').innerHTML = "Computer: '${computerScore}'"; 
 }
